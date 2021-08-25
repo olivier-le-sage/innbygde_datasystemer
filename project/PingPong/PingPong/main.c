8aa85133b38b12a796e201706e3c1a32b9d11b94
olivier-le-sage <olivierlesage86@gmail.com>
2021-08-26 00:40:02 +0200

    main: initial commit with base project set up + draft main.c

---
+++ project/PingPong/PingPong/main.c
@@ -0,0 +1,18 @@
+/*
+ * ping_pong.c
+ *
+ * Created: 2021-08-25 11:56:40 PM
+ */
+
+#include "ping_pong.h"
+#include "rs232.h"
+
+int main(void)
+{
+	assert(!uart_init());
+	
+	printf("UART test.\n");
+
+	for (;;);
+}
+
---
+++ project/PingPong/PingPong/ping_pong.h
@@ -0,0 +1,8 @@
+/*
+ * Project: Ping pong game with a distributed embedded control system
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <avr/io.h>
